d91c0beb5155975eaa6c5a14b1d5d757892fddcb
vaishnavi960 <84568982+vaishnavi960@users.noreply.github.com>
2021-05-25 23:37:48 +0530

    Add files via upload

---
+++ project8/PlayerSort.java
@@ -0,0 +1,21 @@
+package project8;
+
+import java.util.TreeSet;
+
+class PlayerSort {
+	 public static void main(String args[])
+	 {
+		 System.out.println("Player Details After Sorting:");
+	     TreeSet<Player> play= new TreeSet<>(new Player());
+	    // add objects to the TreeSet
+	    play.add(new Player("Abhay",28,"Hockey"));
+	    play.add(new Player("Arun",22,"Golf"));
+	    play.add(new Player("Ajay",25,"Football"));
+	    // adding an object with same age
+	    play.add(new Player("Amar",22,"Hockey"));
+	     // printing the TreeSet
+	     for (Player p : play) {
+	         System.out.println(p);
+	     }
+	 }
+}
---
+++ project9/Order.java
@@ -0,0 +1,31 @@
+package project9;
+import java.io.Serializable;
+
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String pname;
+    private int id;
+    private transient String cname;
+//this variable will not be serialized
+    
+    Order() {
+    	Order.sort(id);
+    };
+
+    private static void sort(int id2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	Order(String pname, int id, String cname) {
+        this.pname = pname;
+        this.id = id;
+       this.cname = cname;
+    }
+
+    @Override
+    public String toString() {
+        return "ID:" + id + "\nProduct Name: " + pname ;
+    }
+}
