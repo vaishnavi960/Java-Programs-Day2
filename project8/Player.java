7d896929d8822a7343cbbc8f12c52cfdc12c0c58
vaishnavi960 <84568982+vaishnavi960@users.noreply.github.com>
2021-05-26 11:07:43 +0530

    Update Player.java

--- project8/Player.java
+++ project8/Player.java
@@ -1,8 +1,10 @@
 package project8;
 
+import java.lang.*;
+
 import java.util.Comparator;
 
-public class Player implements Comparator<Player> {
+public  class Player implements Comparator<Player> {
 	  
     private String name;
     private int age;
@@ -28,32 +30,39 @@ public class Player implements Comparator<Player> {
     public String getGame() { 
     	return game; 
     	}
+       @Override
+	public int compare(Player p1, Player p2) {
+    
+    	int compar =p1.getGame().compareTo(p2.getGame());
+        
   
-    @Override
-    public  int compare(Player p1, Player p2)
-    {
-        if (p1.getGame().hashCode() == p2.getGame().hashCode()) {
-        	if (p1.getage() == p2.getage()) {
-                return 0;
+            if (compar == 0) {
+            	if (p1.getage() == p2.getage()) {
+                    return 0;
+                }
+                else if (p1.getage() < p2.getage()) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
             }
-            else if (p1.getage() < p2.getage()) {
-                return -1;
-            }
-            else {
-                return 1;
-            }
+            	 
+        if ( compar > 0) {
+            return  1;
         }
-        else if(p1.getGame().hashCode() < p2.getGame().hashCode())
-        {
-        	return 1;
-        }
+ 
         else {
-            return -1;
+        	return -1;
         }
     }
-  
-    @Override public String toString()
+   
+    
+	@Override public String toString()
     {
         return "Name: "+name +" Age: " + age + " Game: "+game;
     }
+	
+		// TODO Auto-generated method stub
+	
 }
